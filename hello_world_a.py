a40e41e119c08adf1db5a168fad88985adfcf201
Dean Thomas <dthomas@pixalytics.com>
2019-05-07 14:16:14 +0100

    Added calls to functions in 'submodules_test-c'.

--- hello_world_a.py
+++ hello_world_a.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from submodules_test_b import hello_world_b as b
+from submodules_test_c import hello_world_c as c
 
 def hello():
     print("Hello from submodule a")
@@ -10,4 +11,8 @@ if __name__ == "__main__":
     hello()
     b.hello()
     b.something_else()
+    b.another_thing()
+    c.hello()
+    c.some_function()
+    c.another_function()
     
