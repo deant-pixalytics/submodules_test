0f92e229ff1d1c79102c6e4e06827f1b2efadda5
Dean Thomas <dthomas@pixalytics.com>
2019-05-07 11:58:20 +0100

    Called the 'hello_world' function in submodule b.

--- hello_world_a.py
+++ hello_world_a.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python
 
+from submodules_test_b import hello_world_b as b
+
 def hello():
     print("Hello from submodule a")
+
+
+if __name__ == "__main__":
+    hello()
+    b.hello()
+    
